00e9059800df3ec83e9184de57ceea5fd6f1d5e0
Unaimend <Unaimend@gmail.com>
2016-01-21 18:25:17 +0100

    Utility.h hinzugefuegt

---
+++ include/utility.h
@@ -0,0 +1,45 @@
+/************************************
+*Author: 	Thomas Dost
+*Datum: 	22.12.2015
+*Beschr.	Datei fuer alle VUtility Funktionen
+*Changelog:
+			21.01.2016
+*			Erstellt
+*TODO:
+			std::hash ersetzen
+			bool ListenToGameEvent(std::string)
+			void ListenToGameEvent(std::string, <Function>)	
+				z.B Wird von Lua gecalled, gibt bool aus Lua Stack falls true <Function> ausfuehren	
+					function LevelUpMessage (eventInfo)
+    						 Say(nil, "Someone just leveled up!", false)
+ 					end
+ 
+ 					function Activate ()
+    					 ListenToGameEvent("dota_player_gained_level", LevelUpMessage, nil)
+ 					end
+*************************************/
+#pragma once
+#include <functional> //std::hash
+#include <string>
+	
+namespace eng
+{
+	namespace util
+	{
+		/***********************************
+		*Descri:	Hash Wrapper
+		*Param1:	String der gehashed werden soll
+		*Return:	String als hash
+		************************************/
+		inline size_t toHash(const std::string& string1)
+		{
+			return std::hash<std::string>()(string1);
+		}
+
+		inline bool listenToGameEvent(const std::string& eventName)
+		{
+			;
+		}
+	}
+}
+	
