559f96ede43f94f731c13285fea8bdf28229cc89
tdOSX <Unaimend@gmail.com>
2016-04-05 10:35:13 +0200

    XmlElement hinzugefuegt und angefangen zu implementieren
    XmlElement hinzugefuegt
                Conversion Konstruktor fuer XmlElement hinzugefuegt
                Funktion aus tinyxml2 zu eigener Klasse hinzugefuegt
                --operator und ++ operator hinzugefuegt
                getText hinzugefuegt
                isNull hinzugefuegt

--- include/xmlWrapper/Xml.h
+++ include/xmlWrapper/Xml.h
@@ -3,7 +3,18 @@
  *Datum:     04.04.2015
  *Beschr.    Wrapper um tinyxml2 um einfachere Bedienung zu ermoeglichen
  *Changelog:
+            Klasse ist nun Funktionsfaehig
+            XmlElement hinzugefuegt
+            Conversion Konstruktor fuer XmlElement hinzugefuegt
+            Funktion aus tinyxml2 zu eigener Klasse hinzugefuegt
+            --operator und ++ operator hinzugefuegt
+            getText hinzugefuegt
+            isNull hinzugefuegt
  *TODO:
+            Warum funktioniert der Parse Check nicht?
+            XmlNode zugriff returnt bool oder etaws passenderes aka KEINE exception
+            Methoden der tinyxml XMLElement Klasse implementieren
+                http://www.grinninglizard.com/tinyxml2docs/classtinyxml2_1_1_x_m_l_element.html
  *************************************/
 #pragma once
 #include <string>
@@ -21,22 +32,23 @@ namespace eng
     public:
         Xml(filepath filepath)
         {
-            if(mDoc.LoadFile(filepath.c_str()) != XML_NO_ERROR)
+            
+            if(mDoc.LoadFile(filepath.c_str()) == XML_NO_ERROR)
             {
-                if(mDoc.Parse(filepath.c_str()) != XML_NO_ERROR)
-                {
-                    
-                }
-                else
-                {
-                    std::cerr << "Die Datei:" << filepath << "konnte nicht GEPARSED werden" << std::endl;
-                    std::cerr << mDoc.GetErrorStr1() << std::endl;
-                    std::cerr << mDoc.GetErrorStr2() << std::endl;
-                }
+//                if(mDoc.Parse(filepath.c_str()) == XML_NO_ERROR)
+//                {
+//                    
+//                }
+//                else
+//                {
+//                    std::cerr << "Die Datei:" << filepath << " konnte nicht GEPARSED werden" << std::endl;
+//                    std::cerr << mDoc.GetErrorStr1() << std::endl;
+//                    std::cerr << mDoc.GetErrorStr2() << std::endl;
+//                }
             }
             else
             {
-                std::cerr << "Die Datei:" << filepath << "konnte nicht GELADEN werden" << std::endl;
+                std::cerr << "Die Datei:" << filepath << " konnte nicht GELADEN werden" << std::endl;
             }
             
         }
@@ -44,10 +56,56 @@ namespace eng
         {
             mDoc.Print();
         }
-    
-    private:
+        
+    public:
         XMLDocument mDoc;
         
     
+    };
+    
+    class XmlElement
+    {
+    public:
+        XmlElement(XMLElement& rhs)
+        {
+            mNode = &rhs;
+        }
+        
+        XmlElement(XMLElement* rhs)
+        {
+            mNode = rhs;
+        }
+        
+        std::string getText()
+        {
+            if(mNode != nullptr)
+            {
+                return mNode->GetText();
+            }
+        }
+        
+        XmlElement& operator++()
+        {
+            mNode = mNode->NextSiblingElement();
+            return *this;
+        }
+        
+        XmlElement& operator--()
+        {
+            mNode = mNode->PreviousSiblingElement();
+            return *this;
+        }
+        
+        bool isNull()
+        {
+            if(mNode == nullptr)
+            {
+                return true;
+            }
+            return false;
+        }
+    public:
+        XMLElement* mNode;
+       
     };
 }
