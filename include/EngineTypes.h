211ce71712f08e4c4b3b13a19c961ecd6881f1d7
tdOSX <Unaimend@gmail.com>
2016-06-15 10:20:46 +0200

    s. LOG

--- include/EngineTypes.h
+++ include/EngineTypes.h
@@ -6,6 +6,8 @@
 			22.12.205
 *			Typedef fuer int8_t hinzugefuegt
 *           Typedef fuer uint32_t hinzugefuegt.
+*           14.06.2016
+*           filepath von std::string zu const std::string& geaendert
 *TODO:
 *************************************/
 
@@ -18,7 +20,7 @@ typedef int_fast8_t int8;
 typedef int_fast32_t int32;
 typedef uint_fast32_t uint32;
 typedef int_fast64_t int64;
-typedef std::string filepath;
+typedef const std::string& filepath;
 namespace eng
 {
 	typedef size_t hash;
