833151415ecc6f31695105cbc7580c8e9df7c354
tdOSX <Unaimend@gmail.com>
2016-06-19 13:48:40 +0200

    s. Log

--- include/graphicWrapper/Rectangle.h
+++ include/graphicWrapper/Rectangle.h
@@ -7,38 +7,83 @@
 //           Grundfunktionalitaet hinzugefuegt
 //15.06.2016
 //           Rectangle(const eng::Vector2f& pos) hinzugefuegt
-//
+//19.06.2016
+//           Funktionen in cpp ausgelagert
+//           Alles auskommentiert
 //TODO:
-//			eng::Graphics.hpp hinzufuegen
+//
 //***************************************************************
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp> //sf::Rectangle
 
-#include "Vector.h"
-#include <iostream>
+#include "Vector.h" //eng::Vector
+
 #pragma once
 namespace eng
 {
     class Rectangle
 	{
     public:
-        Rectangle(int x, int y)
-        {
-            mRectangle.setPosition(x, y);
-            mRectangle.setSize(sf::Vector2f(20,20));
-        }
-        Rectangle(const eng::Vector2f& pos)
-        {
-//           std::cout << "RECTAGLE CONSTRUKTOR"<< pos.x << " " << pos.y<<std::endl;
-            mRectangle.setPosition(pos.x, pos.y);
-            mRectangle.setSize(sf::Vector2f(20,20));
-        }
-        void draw(sf::RenderWindow& window) const
-        {
-            window.draw(mRectangle);
-        }
+        
+        
+        /**********************************************
+         *Descr:    Default-Konstruktor
+         ***********************************************/
+        Rectangle();
+       
+        /**********************************************
+         *Descr:    1. Konstruktor, delegiert and 3. Konstruktor
+         *Param1:   X-Koordinate des Rechtecks
+         *Param2:   Y-Koordinate des Rechtecks
+        ***********************************************/
+        Rectangle(float x, float y);
+        
+        
+        /**********************************************
+         *Descr:    2. Konstruktor, delegiert and 3. Konstruktor
+         *Param1:   X und Y-Koordinate des Rechtecks
+         ***********************************************/
+        Rectangle(const eng::Vector2f& pos);
+        
+        
+        /**********************************************
+         *Descr:    3. Konstruktor
+         *Param1:   X und Y-Koordinate des Rechtecks
+         ***********************************************/
+        Rectangle(const eng::Vector2f& pos, const eng::Vector2f& size);
+
+        
+        /**********************************************
+         *Descr:    Copy-Konstuktor
+         *Param1:   Zu kopierendes Objekt
+         ***********************************************/
+        Rectangle(const Rectangle& rhs);
+        
+        
+        /**********************************************
+         *Descr:    Copy-Assignment-Operator
+         *Param1:   Zu kopierendes Objekt
+         ***********************************************/
+        Rectangle& operator=(const Rectangle& rhs);
+    
+        /**********************************************
+         *Descr:    Funktion um Rectangle zu drawn
+         *Param1:   Fenster in das gedrawed werden soll
+         ***********************************************/
+        void draw(sf::RenderWindow& window) const;
+        
+        
+        /**********************************************
+         *Descr:    Funktion um Rechteck zu bewegen
+         *Param1:   X und Y-Richtung in die bewegt werden soll
+         ***********************************************/
         void move(const eng::Vector2f& vec);
     public:
+        //Das eigentliche Rechteck
         sf::RectangleShape mRectangle;
+        //Standardbreite
+        const float DEFAULT_WIDTH = 20;
+        //Standardhoehe
+        const float DEFAULT_HEIGHT = 20;
 	};
     
     
--- src/graphicWrapper/Rectangle.cpp
+++ src/graphicWrapper/Rectangle.cpp
@@ -1,10 +1,39 @@
 #include "../../include/graphicWrapper/Rectangle.h"
-#include "../../include/eventSystem/Event.h"
-#include "../../include/Variant.h"
-#include "../../include/globals.h"
+
+
+
+eng::Rectangle::Rectangle() : Rectangle({0,0}, {DEFAULT_WIDTH,DEFAULT_HEIGHT}) {}
+
+
+eng::Rectangle::Rectangle(float x, float y) : Rectangle({x,y},{DEFAULT_WIDTH, DEFAULT_HEIGHT}) {}
+
+
+eng::Rectangle::Rectangle(const eng::Vector2f& pos) : Rectangle(pos,{DEFAULT_WIDTH, DEFAULT_HEIGHT}) {}
+
+
+eng::Rectangle::Rectangle(const eng::Vector2f& pos, const eng::Vector2f& size)
+{
+    mRectangle.setPosition(pos.x, pos.y);
+    mRectangle.setSize(sf::Vector2f(DEFAULT_WIDTH,DEFAULT_HEIGHT));
+}
+
+eng::Rectangle::Rectangle(const Rectangle& rhs) : mRectangle(rhs.mRectangle){};
+
+
+eng::Rectangle& eng::Rectangle::operator=(const eng::Rectangle& rhs)
+{
+    mRectangle = rhs.mRectangle;
+}
+
+
+void eng::Rectangle::draw(sf::RenderWindow& window) const
+{
+    window.draw(mRectangle);
+}
 
 
 void eng::Rectangle::move(const eng::Vector2f& vec)
 {
         mRectangle.move(vec.x, vec.y);
 }
+
