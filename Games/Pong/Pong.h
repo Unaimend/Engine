1e0f1302db30ca282bc84d3527cf7a066e793123
tdOSX <Unaimend@gmail.com>
2017-01-28 13:46:23 +0100

    Added Paddleclass

--- Games/Pong/Pong.h
+++ Games/Pong/Pong.h
@@ -14,7 +14,32 @@
 *TODO:
 *************************************/
 #include "../Game.h"
+#include "../../include/entities/Entity.h"
+#include "../../include/globals.h"
 #include <iostream>
+
+class Paddle
+{
+public:
+    Paddle() = default;
+    Paddle(const eng::Vector2f& pos, const eng::Vector2f& size)
+    {
+        mRectangle = eng::Rectangle(pos, size);
+    }
+
+    eng::Rectangle mRectangle;
+
+    void draw(eng::Window& window)
+    {
+        mRectangle.draw(window.getSfRenderWindow());
+    }
+
+
+
+};
+
+
+
 
 class Pong : protected Game{
 
@@ -27,15 +52,54 @@ public:
     virtual bool init() override
     {
         eng::Xml options(std::string(gFilePath) + "/Games/Pong/options.xml");
+        mRenderWindow->setSize({std::stoi(options.rootElement()["resX"].getValue()), std::stoi(options.rootElement()["resY"].getValue())});
+        return true;
+    }
+
+
+    Paddle leftPaddle;
+
+
+    virtual void start() override
+    {
+
+
+
+        float paddleHeight = (mRenderWindow->getSize().y / 8);
+        float paddleWidth =   (mRenderWindow->getSize().x / 40);
+        leftPaddle = Paddle({0,(float) mRenderWindow->getSize().y - ((mRenderWindow->getSize().y/2)+(paddleHeight/2))}, {paddleWidth,paddleHeight});
+        while(mRenderWindow->isOpen())
+        {
+            switch(mGameState)
+            {
+                case STATE::IN_MENUE:
+                {
+                    break;
+                }
+                case STATE::IN_GAME:
+                {
+                    update();
+                    break;
+                }
+                case STATE::PAUSED:
+                {
+                    std::cout << "PAused" << std::endl;
+                    break;
+                }
+            }
+
+            event();
+            update();
+            render();
+
+        }
 
 
     }
 
 
-    virtual void start() override
+    virtual void event() override
     {
-        while (mRenderWindow->isOpen())
-        {
             sf::Event event;
 
             eng::util::KeyChecker KeyChecker(event);
@@ -43,23 +107,23 @@ public:
             {
                 if (event.type == sf::Event::Closed)
                 {
-
+                    mRenderWindow->close();
                 }
                 if (KeyChecker.keyPressed(sf::Keyboard::D))
                 {
-
+                    std::cout << leftPaddle.mRectangle.getPosition().x <<  "   " <<  leftPaddle.mRectangle.getPosition().y<< std::endl;
                 }
-                if (KeyChecker.keyPressed(sf::Keyboard::A))
+                if (KeyChecker.keyPressed(sf::Keyboard::Escape))
                 {
-
+                    mGameState = mGameState == STATE::PAUSED ? STATE::IN_GAME : STATE::PAUSED;
                 }
                 if (KeyChecker.keyPressed(sf::Keyboard::W))
                 {
-                    mRenderWindow->close();
+                    leftPaddle.mRectangle.move({0, -20});
                 }
                 if (KeyChecker.keyPressed(sf::Keyboard::S))
                 {
-
+                    leftPaddle.mRectangle.move({0, 20});
                 }
                 if (event.type == sf::Event::MouseEntered)
                 {
@@ -68,15 +132,25 @@ public:
             }
 
 
-            mRenderWindow->clear();
-
-            mRenderWindow->display();
-        }
     }
 
+    virtual void render() override
+    {
+        mRenderWindow->clear();
+        leftPaddle.draw(*mRenderWindow);
+        mRenderWindow->display();
+    }
 
+    virtual void update() override
+    {
+//        std::cout << mRenderWindow->getSfRenderWindow().getSize().x << std::endl;
+    }
 
 };
+
+
+
+
 
 
 #endif //ENGINE_PONG_H
