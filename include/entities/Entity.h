4b97f9454fd62977ad5100c2645d719a3e11369d
tdOSX <Unaimend@gmail.com>
2017-01-31 14:56:52 +0100

    Siehe changelog.

--- include/entities/Entity.h
+++ include/entities/Entity.h
@@ -8,8 +8,13 @@ Changelog:
             Auskommentiert
 20.01.2016
             copy-assignment geloescht da er nicht korrekt funktioniert
+27.01.2017
+            Neuen Konstruktor hinzugefügt der nur eine Positon übernimmt
+            #Ifdef SFML um void sf_event()
+
 TODO:
             Xml mData wird nicht kopiert im Assignment Operator
+            sf::Sprite Wrapper schreiben und hier eine Sprite Variarble hinzufuegen.
 *************************************/
 
 
@@ -21,16 +26,15 @@ TODO:
 #include "../graphicWrapper/Vector.h"   //eng::Vector
 #include "../graphicWrapper/Rectangle.h"   //eng::Rectangle
 #include "../xmlWrapper/Xml.h"          //eng::Xml
-#include "../globals.h"             //gLuastate, gEventQueue
-#include "../eventSystem/Event.h"
+#include "../globals.h"             //gLuastate, gEventQueue, SFML
+#include "../eventSystem/Event.h"   //eng::Event
+#include "../graphicWrapper/Sprite.h" //eng::Sprite
 
 #pragma once
 namespace eng
 {
     class Entity
     {
-
-    
     public:
         /**********************************************
          *Descr:   Konstruktor um Entity zu initialisieren
@@ -38,7 +42,14 @@ namespace eng
          *Param2:   LuaState indem die Entity ablaeuft
          *Param3:   XmlState fuers Laden und Speichern der Entity
          ***********************************************/
-        Entity(const eng::Vector2f& vec, lua::LuaState& state, const eng::Xml& data);
+        Entity(const eng::Vector2f& pos, lua::LuaState& state, const eng::Xml& data);
+
+
+        /**********************************************
+         *Descr:   Konstruktor um Entity zu initialisieren
+         *Param1:   Position der Entity
+         ***********************************************/
+        Entity(const eng::Vector2f& pos);
         
         
         /**********************************************
@@ -58,20 +69,40 @@ namespace eng
         //Virtuelle Funktion fuer Vererbungsbaum
         virtual void render(sf::RenderWindow& window) const = 0;
         virtual void update(float framtime) = 0;
-        virtual void eng_update(const eng::Event& event) = 0;
-        virtual void sf_update(const sf::Event& event) = 0;
+        virtual void eng_event(const eng::Event &event) = 0;
 
+#ifdef SFML
+        virtual void sf_event(const sf::Event &event) = 0;
+#endif
     private:
+        eng::Sprite mSprite;
         //Position der Entity
-        eng::Vector2f mPos{0,0};
+        eng::Vector2f mPos{mSprite.getPosition().x,mSprite.getPosition().y};
         //Verweis auf den LuaState der jweiligen Entity
+        //TODO Die Referenzen durch Pointer oder "normale" Varaiblen austauschen
         lua::LuaState& mState;
         //Daten der Entity
         const eng::Xml& mData;
+
+
     
     };
+
+
     static eng::Event ev1{"EntitiyDead",{"TEXT"},{ {eng::Variant::Type::INTEGER, 300} }};
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Wird NICHT Refactored da Test Entity
     class RecEntity : public Entity
     {
@@ -99,23 +130,18 @@ namespace eng
 
         virtual void update(float frametime) override
         {
-//
-//            if(mRec.mRectangle.getPosition().x > 300)
-//            {
-//                gEventQueue.addEvent(ev1);
-//              
-//            }
+            ;
         }
-        virtual void eng_update(const eng::Event& event) override
+        virtual void eng_event(const eng::Event &event) override
         {
             ;
         }
-        virtual void sf_update(const sf::Event& event) override
+        virtual void sf_event(const sf::Event &event) override
         {
             ;
         }
     };
-    
+
 }
 
 
